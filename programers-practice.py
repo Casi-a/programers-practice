67da7a722c5b7d9e225c82f2bbb3d578779cef49
DESKTOP-T2QF0SO\User <wlsehmd321@gmail.com>
2021-04-25 11:34:58 +0900

    프로그래머스 '파이썬을 파이썬답게' 학습 내용 1일차 https://programmers.co.kr/learn/courses/4008

---
+++ programers-practice.py
@@ -0,0 +1,37 @@
+# # 이렇게 코딩하면 파이썬 스타일이 아니다! 명심!
+# def solution(mylist):
+#     answer = []
+#     for i in range(len(mylist)):
+#         answer.append(len(mylist[i]))
+
+
+# # 몫과 나머지를 이쁘게 만들 수 있다
+# a = 7
+# b = 5
+# print(*divmod(a, b))
+
+
+# # 진법 변환, num은 str이어야 한다
+# num, base = map(int, input().strip().split(' '))
+# print(int(str(num), base))
+
+
+# # ljust() : 문자열 왼쪽 정렬
+# # center() : 가운데 정렬
+# # rjust() : 오른쪽 정렬
+# s, n = input().strip().split(' ')
+# n = int(n)
+# print(s.ljust(n))
+# print(s.center(n))
+# print(s.rjust(n))
+
+# # 0 입력시 소문자 알파벳, 1 입력시 대문자 알파벳 표시
+# # https://docs.python.org/3.4/library/string.html
+# # 더 많은 상수 찾기
+# # 파이썬에서는 상수를 정의하여 쓴다.
+# import string
+# num = int(input().strip())
+# if num == 0:
+#     print(string.ascii_lowercase)
+# elif num == 1:
+#     print(string.ascii_uppercase)
